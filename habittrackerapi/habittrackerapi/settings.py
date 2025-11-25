40a6ee3eeaa536db395bb970b7b7876701c0467e
Trần Trung Hậu <129314970+TrungHau810@users.noreply.github.com>
2025-11-25 23:49:43 +0700

    Change DEBUG setting to True for development

--- habittrackerapi/habittrackerapi/settings.py
+++ habittrackerapi/habittrackerapi/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g+*cuw%=foj(ft2i(=71dmn!y4sq3rw70+uqt9ycvy!^c82-9w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
