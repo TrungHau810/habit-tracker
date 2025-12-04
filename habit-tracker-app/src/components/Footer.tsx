f18f30e4b56f82cf6a11f57ebbb13f9f844ce016
trunghau810 <tthau2004@gmail.com>
2025-12-04 22:53:42 +0700

    Add new react project

--- habit-tracker-app/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="w-full bg-gray-100 py-4">
-            <div className="max-w-7xl mx-auto px-4 text-center text-gray-600">
-                &copy; {new Date().getFullYear()} Habit Tracker. All rights reserved.
-            </div>
-        </footer>
-    );
-}
