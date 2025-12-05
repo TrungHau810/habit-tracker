5ac6e1675d5ee90d4d8bc8aa84cc59d944fa3c0e
trunghau810 <tthau2004@gmail.com>
2025-12-05 12:40:51 +0700

    Fix: Unauthenticated for sign-up API

--- habittrackerapi/trackerapp/views.py
+++ habittrackerapi/trackerapp/views.py
@@ -21,6 +21,13 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView, generics.UpdateAPIVi
     serializer_class = serializers.UserSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_permissions(self):
+        if self.action in ['create']:
+            self.permission_classes = []
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
     @action(methods=["get"], detail=False, url_path='me', url_name='me', permission_classes=[IsAuthenticated])
     def get_current_user(self, request):
         serializer = self.get_serializer(request.user)
